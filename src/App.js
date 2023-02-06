495a96268b1cedc82c0f5aea87c209522d2f1ffa
huyhoangm2 <hoangken760@gmail.com>
2023-02-07 00:15:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [userInput, setUserInput] = useState("");
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+  const handleButtonClick = (e) => {
+    alert(userInput)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" name="user" placeholder="Please type somthing" 
+      onChange={handleInputChange} />
+      <button onClick={handleButtonClick}>Print</button>
     </div>
   );
 }
 
-export default App;
+
